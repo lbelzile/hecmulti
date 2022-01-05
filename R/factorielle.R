c3c5c368e2ec67e2e378ed93e7887679b087d244
Léo Belzile <belzilel@gmail.com>
2022-01-05 12:48:45 -0500

    Analyse facto (vignette et fonctions), ggplot2

--- R/factorielle.R
+++ R/factorielle.R
@@ -1,6 +1,5 @@
 # Méthodes S3 de base pour objects de classe "factanal"
 #' @import stats
-#' @importFrom graphics abline
 #' @export
 logLik.factanal <- function(object, ...) {
   # Log-vraisemblance de la loi de Wishart
@@ -31,3 +30,124 @@ AIC.factanal <- function(object, k = 2, ...){
 BIC.factanal <- function(object, ...){
   AIC.factanal(object, k = log(object$n.obs))
 }
+
+#' Alpha de Cronbach
+#'
+#' Calcul de la mesure de cohérence interne
+#' pour la création d'échelles
+#'
+#' @param x matrice de variables numériques
+#' @return alpha de Cronbach
+#' @export
+alpha_Cronbach <- function(x){
+  S2 <- var(rowSums(x))
+  ncol(x)/(ncol(x)-1)*(S2-sum(apply(x, 2, var)))/S2
+}
+
+
+#' Correction de Bartlett pour analyse factorielle
+#'
+#' Calcule une correction de Bartlett pour l'analyse factorielle
+#' @param object objet de class \code{factanal}
+#' @return une liste avec les éléments suivants:
+#' \describe{
+#' \item{\code{statistic}}{ statistique du rapport de vraisemblance fois correction de Bartlett}
+#' \item{\code{pvalue}}{valeur-p pour la statistique modifiée}
+#'}
+#' @export
+bartlettcorr_factanal <- function(object){
+  if(!isTRUE(inherits(object, what = "factanal"))){
+    stop("M\u00e9thode valide uniquement pour les objets de classe `factanal`")
+  }
+p <- length(object$uniquenesses)
+k <- object$factors
+n <- object$n.obs
+lrt <- object$STATISTIC *
+  (1 - (2*p + 5 + 4* k)/(6 * n - 1))
+pval <- pchisq(lrt,
+               object$dof,
+               lower.tail = FALSE)
+list(statistic = as.numeric(lrt),
+     pvalue = as.numeric(pval))
+}
+
+
+#' Critères d'information pour analyse factorielle
+#'
+#' Cette fonction calcule les estimation
+#' du maximum de vraisemblance pour une
+#' analyse factorielle.
+#'
+#' @export
+#' @param factors le nombre de facteurs à ajuster (vecteur)
+#' @param ... autres arguments passés à factanal
+crit_emv_factanal <- function(factors, ...){
+ stopifnot("Nombre de facteurs manquants" = !missing(factors))
+factors <- sort(as.integer(factors))
+emv_crit <- function(k, ...){
+  fai <- factanal(factors = k, ...)
+  stopifnot("Pas de nombre d'observations" = !is.null(fai$n.obs))
+  c(k,
+    AIC(fai),
+    BIC = BIC(fai),
+    as.numeric(fai$PVAL),
+    nobs(fai),
+    isTRUE(any(fai$uniquenesses == 0.005)))
+}
+res <- t(sapply(X = factors,
+                FUN = emv_crit,
+                ... = ...))
+data.frame(k = as.integer(res[,1]),
+           AIC = res[,2],
+           BIC = res[,3],
+           pval = base::format.pval(res[,4], digits = 4),
+           npar = as.integer(res[,5]),
+           heywood = res[,6])
+}
+
+#' Diagramme d'éboulis
+#'
+#' Crée un diagramme d'éboulis pour les
+#' objets de class \code{eigen}
+#' @export
+#' @import ggplot2 patchwork
+#' @param object objet de classe \code{eigen}
+#' @param which vecteur d'entiers indiquant quels graphiques imprimer, soit \code{1} pour le diagramme d'éboulis et \code{2} pour la variance cumulative
+#' @return une matrice de deux objets \code{ggplot}
+eboulis <- function(object, which = 1:2){
+  stopifnot(isTRUE(inherits(x = object, what = "eigen")),
+            isTRUE(length(object$values) > 1))
+  valpropres <- object$values
+  df <- data.frame(y = valpropres,
+                  x = seq_along(valpropres))
+  g1 <-  ggplot2::ggplot(data = df,
+                mapping = ggplot2::aes_string(x = "x",
+                                              y = "y")) +
+    ggplot2::geom_line() +
+    ggplot2::geom_point() +
+    ggplot2::scale_x_continuous(breaks = seq_along(valpropres)) +
+    ggplot2::labs(x = "nombre de composantes",
+         y = "valeurs propres",
+         subtitle = "diagramme d'\u00e9boulis") +
+    ggplot2::theme_classic()
+  g2 <-  ggplot2::ggplot(data = data.frame(y = cumsum(valpropres),
+                                           x = seq_along(valpropres)),
+                         mapping = ggplot2::aes_string(x = "x", y = "y")) +
+    ggplot2::geom_line() +
+    ggplot2::geom_point() +
+    ggplot2::scale_x_continuous(breaks = seq_along(valpropres)) +
+    ggplot2::labs(x = "nombre de composantes",
+                  y = "variance cumulative") +
+    ggplot2::theme_classic()
+  stopifnot(isTRUE(all(which %in% c(1, 2))))
+  if(length(which) == 1){
+   if(which == 1){
+    g1
+  } else if(which == 2){
+    g2
+  }
+  } else{
+    patchwork::wrap_plots(list(g1, g2))
+  }
+}
+
--- R/logistique.R
+++ R/logistique.R
@@ -1,4 +1,5 @@
 
+
 #' Courbe lift
 #'
 #' Pour un vecteur de probabilitées (prédictions) d'un modèle, \code{prob},
@@ -6,23 +7,68 @@
 #'
 #' @param prob vecteur de probabilités estimées
 #' @param resp variable réponse binaire
+#' @param plot logique; si \code{TRUE}, retourne un graphique de la courbe lift
+#' @param levels niveaux des points de coupure
+#' @return un graphique \code{ggplot} de la courbe lift et une base de données avec la valeur du lift
+#'
+#'@details Le nombre de données classifiées est arrondi, contrairement au graphique
 #' @export
-lift <- function(prob, resp){
+lift <- function(prob,
+                 resp,
+                 plot = TRUE,
+                 levels = seq(0.1, 0.9, by = 0.1)) {
   od <- order(prob, decreasing = TRUE)
-  #Réordonner les observations
+  # Réordonner les observations
   resp <- resp[od]
   n <- length(resp)
-  prand <- seq(1, sum(resp), length.out = length(resp))
-  slift <- cumsum(resp)/prand
-  plot(x = prand/sum(resp), y= cumsum(resp)/sum(resp),
-       ylim = c(0, 1), xlim = c(0, 1), yaxs= "i",
-       xaxs = "i",
-       bty = "l",
-       type= "s",
-       panel.first = abline(a = 0, b = 1, lty = 2),
-       xlab = "Pourcentage de positifs class\u00e9s",
-       ylab = "Pourcentage de positifs d\u00e9tect\u00e9s")
-  return(slift)
+  prand <- seq(from = 1,
+               to = sum(resp),
+               length.out = length(resp))
+  slift <- cumsum(resp) / prand
+  if (plot) {
+    g1 <- ggplot2::ggplot(
+      data.frame(
+        x = 100 * prand / sum(resp),
+        y = 100 * cumsum(resp) / sum(resp)
+      ),
+      mapping = ggplot2::aes_string(x = "x",
+                                    y = "y")
+    ) +
+      ggplot2::geom_abline(slope = 1,
+                           intercept = 0,
+                           linetype = 2) +
+      ggplot2::geom_line() +
+      ggplot2::geom_point() +
+      ggplot2::scale_x_continuous(
+        breaks = seq(0L, 100L, by = 20L),
+        minor_breaks = seq(10L, 90L, by = 10L),
+        labels = c("0", "20", "40", "60", "80", "100"),
+        limits = c(0, 100),
+        expand = c(0, 0)
+      ) +
+      ggplot2::scale_y_continuous(
+        breaks = seq(0L, 100L, by = 20L),
+        minor_breaks = seq(10L, 90L, by = 10L),
+        labels = c("0", "20", "40", "60", "80", "100"),
+        limits = c(0, 100),
+        expand = c(0, 0)
+      ) +
+      ggplot2::labs(x = "pourcentage de positifs correctement class\u00e9s",
+                    y = "pourcentage de positifs d\u00e9tect\u00e9s",
+                    subtitle = "courbe lift") +
+      ggplot2::theme_classic()
+    print(g1)
+  }
+  # Calcul du lift
+  qu <- quantile(seq_along(resp), levels)
+  hasard <- round(qu * mean(resp))
+  modele <- cumsum(resp)[round(qu)]
+  data.frame(
+    pourcent = as.integer(seq(10L, 90L, by = 10L)),
+    hasard = as.integer(hasard),
+    modele = as.integer(modele),
+    lift = modele / hasard
+  )
 }
 
 
@@ -43,11 +89,11 @@ lift <- function(prob, resp){
 #' \item{\code{fneg}}{taux de faux négatifs}
 #' }
 #' @export
-perfo_logistique <- function(prob, resp){
+perfo_logistique <- function(prob, resp) {
   # VRAI == 1, FAUX == 0
   cuts <- seq(from = 0.01, to = 0.99, by = 0.01)
   tab <- data.frame(coupe = cuts,
-                    t(sapply(cuts, function(cut){
+                    t(sapply(cuts, function(cut) {
                       nsucces <- sum(resp == 1)
                       nechec <- length(resp) - nsucces
                       n <- length(resp)
@@ -56,15 +102,20 @@ perfo_logistique <- function(prob, resp){
                       c0 <- sum(!predy & !resp) # Y=0, Yhat=0
                       i1 <- sum(!predy & resp) # Y=1, Yhat=0
                       i0 <- sum(predy & !resp) # Y=0, Yhat=1
-                      c(VP = c1,
+                      c(
+                        VP = c1,
                         VN = c0,
                         FP = i1,
                         FN = i0,
-                        pcorrect = 100*(c0+c1)/n,
-                        sensi = 100*c1/(c1+i1), # Y=1 & Yhat=1 / # Y=1
-                        speci = 100*c0/(c0+i0), # Y=0 & Yhat=0 / # Y=0
-                        fpos = 100*i0/(c1+i0), # Y=0 & Yhat=1 / # Yhat=1
-                        fneg = 100*i1/(c0+i1)) # Y=1 & Yhat=0 / # Yhat=0
+                        pcorrect = 100 * (c0 + c1) / n,
+                        sensi = 100 * c1 / (c1 + i1),
+                        # Y=1 & Yhat=1 / # Y=1
+                        speci = 100 * c0 / (c0 + i0),
+                        # Y=0 & Yhat=0 / # Y=0
+                        fpos = 100 * i0 / (c1 + i0),
+                        # Y=0 & Yhat=1 / # Yhat=1
+                        fneg = 100 * i1 / (c0 + i1)
+                      ) # Y=1 & Yhat=0 / # Yhat=0
                     })))
   tab
 }
@@ -87,52 +138,78 @@ perfo_logistique <- function(prob, resp){
 #' @param ncv entier, nombre de plis pour la validation croisée
 #' @return un graphique de la performance moyenne en fonction du point de coupure et une liste avec les points de coupure \code{ptcoupe}, la valeur optimale du point de coupure \code{opt} et la performance \code{performance}
 #' @export
-pcoupe <- function(model, c00 = 1, c11 = 1, c01 = 0, c10 = 0,
-                   plot = FALSE, nrep = 10L, ncv = 10L){
+pcoupe <- function(model,
+                   c00 = 1,
+                   c11 = 1,
+                   c01 = 0,
+                   c10 = 0,
+                   plot = FALSE,
+                   nrep = 10L,
+                   ncv = 10L) {
   nrep <- as.integer(nrep)
   ncv <- as.integer(ncv)
   stopifnot(nrep > 0, ncv > 1)
   # Vérifier qu'on modélise des données binaires
-  if(inherits(class(model), what = "glm")){
-    if(!model$family$family == "binomial"){
-      stop("Mod\u00e8le lin\u00e9aire g\u00e9n\u00e9ralis\u00e9 n'a pas une variable r\u00e9ponse binomiale.")
+  if (inherits(class(model), what = "glm")) {
+    if (!model$family$family == "binomial") {
+      stop(
+        "Mod\u00e8le lin\u00e9aire g\u00e9n\u00e9ralis\u00e9 n'a pas une variable r\u00e9ponse binomiale."
+      )
     }
   } else{
     stop("Mod\u00e8le invalide.")
   }
-  if(is.null(nrow(model$data))){
-    stop("Mod\u00e8le ajust\u00e9 devrait contenir une base de donn\u00e9es `data` avec les variables explicatives.")
+  if (is.null(nrow(model$data))) {
+    stop(
+      "Mod\u00e8le ajust\u00e9 devrait contenir une base de donn\u00e9es `data` avec les variables explicatives."
+    )
   }
-  model$data$y <- model$data[,all.vars(formula(model))[attr(terms(formula(model)), "response")]]
- stopifnot(length(unique(model$data$y)) != 2)
+  model$data$y <-
+    model$data[, all.vars(formula(model))[attr(terms(formula(model)), "response")]]
+  stopifnot(length(unique(model$data$y)) != 2)
   n <- nrow(model$data)
   perfor_cv <- replicate(n = nrep, expr = {
     #Shuffle the indices
-    inds <- sample.int(n = n, size = n, replace = FALSE)
+    inds <- sample.int(n = n,
+                       size = n,
+                       replace = FALSE)
     #Split into K groups of ~ equal size (from https://stackoverflow.com/a/16275428)
-    form_group <- function(x, n){ split(x, cut(seq_along(x), n, labels = FALSE)) }
+    form_group <-
+      function(x, n) {
+        split(x, cut(seq_along(x), n, labels = FALSE))
+      }
     groups <- form_group(x = inds, n = ncv)
     probs <- rep(0, n)
-    for(j in seq_len(ncv)){
-      probs[groups[[j]]] <- predict(glm(model$formula,
-                                        family = model$family,
-                                        data = model$data[-groups[[j]],]),
-                                    newdata = model$data[groups[[j]],],
-                                    type = "response")
+    for (j in seq_len(ncv)) {
+      probs[groups[[j]]] <- predict(
+        glm(
+          model$formula,
+          family = model$family,
+          data = model$data[-groups[[j]], ]
+        ),
+        newdata = model$data[groups[[j]], ],
+        type = "response"
+      )
     }
     perfo <- perfo(probs, resp = model$y)
-    gain <- perfo$c0 * c00 + perfo$c1 * c11 + perfo$i0 *c10 + perfo$i1 * c01
+    gain <-
+      perfo$c0 * c00 + perfo$c1 * c11 + perfo$i0 * c10 + perfo$i1 * c01
     gain
   })
   meanperfo <- rowMeans(perfor_cv)
   cut <- seq(from = 0.01, to = 0.99, by = 0.01)
-  if(plot){
-    plot(cut, meanperfo,
-         xlab = "coupe",
-         ylab = "gain moyen")
+  if (plot) {
+    g1 <- ggplot2::ggplot(data = data.frame(x = cut,
+                                      y = meanperfo),
+                    mapping = ggplot2::aes_string(x = "x",
+                                                  y = "y")) +
+      ggplot2::geom_point() +
+      ggplot2::labs(x = "coupe",
+                    y = "gain moyen") +
+      ggplot2::theme_classic()
+    print(g1)
   }
   list(ptcoupe = cut,
        performance = meanperfo,
-       opt = cut[which.max(meanperfo)]
-  )
+       opt = cut[which.max(meanperfo)])
 }
